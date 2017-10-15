68e82b7230b88106d34d222a1d8b02ee03e512c6
ruslanzharkov <rus_basket_kg@inbox.ru>
2017-10-15 17:02:11 +0600

    Added add comment method to views.py

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     url(r'^post/(?P<pk>\d+)/publish', views.post_publish, name='post_publish'),
-    url(r'^post/(?P<pk>\d+)/delete/$', views.post_delete, name='post_delete')
+    url(r'^post/(?P<pk>\d+)/delete/$', views.post_delete, name='post_delete'),
+    url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
 ]
