8e30eb561a53e39536c3a55585ae7e1c23094d62
ruslanzharkov <rus_basket_kg@inbox.ru>
2017-10-15 12:45:42 +0600

    Added post form editor

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
+from django.shortcuts import redirect
 
 
 # This method get all posts
@@ -17,5 +18,14 @@ def post_detail(request, pk):
 
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
